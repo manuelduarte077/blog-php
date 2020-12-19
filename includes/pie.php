83f220ad6dc6cef1b977ff7d9c20c25248cb9039
manuelduarte077 <carlosd99m@gmail.com>
2020-12-18 22:25:08 -0600

    Proyecto de blog con php Completado (Listo para cambios)

---
+++ includes/pie.php
@@ -0,0 +1,11 @@
+
+			<div class="clearfix"></div>
+		</div> <!-- fin contenedor -->
+
+		<!-- PIE DE PÁGINA -->
+		<footer id="pie">
+			<p>Desarrollado por Manuel Duarte &copy; 2018</p>
+		</footer>
+		
+	</body>
+</html>
